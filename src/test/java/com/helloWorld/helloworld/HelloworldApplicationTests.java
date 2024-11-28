ac23e9d865051de7f26ea9ee3fc279f5893a19b7
rayane moara <rayane_moara@hotmail.com>
2024-11-28 12:42:50 -0300

    Atividades Spring

---
+++ src/test/java/com/helloWorld/helloworld/HelloworldApplicationTests.java
@@ -0,0 +1,13 @@
+package com.helloWorld.helloworld;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class HelloworldApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
